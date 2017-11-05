9337c097c3b21fed88f1cbf9ac9a74f700b35f14
3two <whitfija@gmail.com>
2017-11-04 20:23:16 -0400

    Creation & Upload
    Hello World is in folder

---
+++ Hello World/HelloWorld 1.py
@@ -0,0 +1,24 @@
+import pygame, sys
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((500,500))
+
+pygame.sprite.Sprite.add
+
+def drawscreen():
+    pygame.draw.rect(screen, (250, 250, 250), (0, 0, 500, 500))
+
+def drawplayer():
+
+
+#pygame.display.set_caption('Hello World!')
+while True: # main game loop
+
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
