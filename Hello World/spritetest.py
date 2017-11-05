68158fe754b93d9a83b11a28f23fc7ee26c16534
DylanDillpickle <33379133+DylanDillpickle@users.noreply.github.com>
2017-11-04 23:09:18 -0400

    asdasd

--- Hello World/spritetest.py
+++ Hello World/spritetest.py
@@ -12,12 +12,14 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load("Enemy.png").convert()
         self.image.set_colorkey(WHITE)
-        pygame.transform.scale2x()
+        def drawplayer(self,surface):
+            self = pygame.transform.scale2x(self.image)
+            surface.blit(self.image, (self.rect.x, self.rect.y))
         #def __init__(self):
         #pygame.sprite.Sprite.__init__(self)
         #self.image = pygame.Surface([30,30])
         #self.image.fill([255,0,0])
-        #self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect()
 
     #def move_player(self):
         #key = pygame.key.get_pressed()
@@ -39,16 +41,17 @@ pygame.init()
 screen = pygame.display.set_mode((500,500))
 pygame.display.set_caption('Sprite Test')
 sprites = pygame.sprite.Group()
-
-for i in range(50):
-    test = Player(RED, 20, 15)
-    test.rect.x = 0
-    test.rect.y = 0
-    sprites.add(test)
+#for i in range(50):
+    #test = Player(RED, 20, 15)
+   # test.rect.x = 0
+  #  test.rect.y = 0
+ #   sprites.add(test)
 while True: # main game loop
     screen.fill(WHITE)
     sprites.draw(screen)
+    Player.drawplayer(screen)
     #test.move_player()
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
