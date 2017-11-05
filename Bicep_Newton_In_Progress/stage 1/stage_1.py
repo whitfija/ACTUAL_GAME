5ee7f4ee4281c7c4e05eaec14ee7635aba38c9f5
3two <whitfija@gmail.com>
2017-11-05 10:34:39 -0500

    tryuyli

--- Bicep_Newton_In_Progress/stage 1/stage_1.py
+++ Bicep_Newton_In_Progress/stage 1/stage_1.py
@@ -1,40 +1,23 @@
-import pygame, sys
+import pygame, sys, math, easygui
 from pygame.locals import *
+import time
 from sys import exit
-from tkinter import Tk, Label, Button
-
-class App:
-    func = 0
-
-#if func == 1:
-    #x ** 1/2
-
-    def __init__(self, master):
-        def funcTo(num):
-            App.func = num
-            print(App.func)
-            master.quit
-            root.withdraw()
-        self.master = master
-        master.title("A simple GUI")
-        self.label = Label(master, text="Choose")
-        self.label.pack()
-        self.greet_button = Button(master, text="SQRT", command= lambda: funcTo(1))
-        self.greet_button.pack()
-        self.greet_button = Button(master, text="Parabola", command= lambda: funcTo(2))
-        self.greet_button.pack()
-        self.greet_button = Button(master, text="Exponential", command= lambda: funcTo(3))
-        self.greet_button.pack()
-        self.close_button = Button(master, text="OK", command=master.quit)
-        self.close_button.pack()
+import tkinter as tk
+import tkinter as tk2
+from tkinter import *
 
 #colors
 BLACK = (0,   0,   0)
 WHITE = (255, 255, 255)
 RED = (255,   0,   0)
 
+#declarations
+hit = False
+coeff = 1
 bg = pygame.image.load("images/grid.png")
 
+
+#sprite classes
 #Player class
 class Player(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
@@ -53,21 +36,17 @@ class Player(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
 
-    #def move_player(self):
-        #key = pygame.key.get_pressed()
-        #if key[pygame.K_DOWN]:
-
 # Enemy class
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         super().__init__()
-    # find image
+        # find image
         self.image = pygame.image.load("images/Enemy.png").convert()
-    # background?
+        # background?
         self.image.set_colorkey(WHITE)
-    # scale attempt
+        # scale attempt
         # pygame.transform.scale2x(self.image)
-    # define rect
+        # define rect
         self.rect = self.image.get_rect()
 
 # Ball class
@@ -83,53 +62,205 @@ class Ball(pygame.sprite.Sprite):
         # define rect
         self.rect = self.image.get_rect()
 
-#start pygame & set display
-pygame.init()
-screen = pygame.display.set_mode((1024,768))
+# Enemy_Hit class
+class Enemy_Hit(pygame.sprite.Sprite):
+    def __init__(self, color, width, height):
+        super().__init__()
+        # find image
+        self.image = pygame.image.load("images/fire.png").convert()
+        # background?
+        self.image.set_colorkey(WHITE)
+        # scale attempt
+        # pygame.transform.scale2x(self.image)
+        # define rect
+        self.rect = self.image.get_rect()
+
+# start pygame & set display
+screen = pygame.display.set_mode((1024, 768))
 pygame.display.set_caption('Level 1')
-#create sprites group
+# create sprites group
 sprites = pygame.sprite.Group()
+hitsprites = pygame.sprite.Group()
 
 #add player to group
 for i in range(50):
-    p1 = Player(RED, 20, 15)
-    p1.rect.x = 10
-    p1.rect.y = 768-45-120
-    sprites.add(p1)
+   player = Player(RED, 20, 15)
+   player.rect.x = 10
+   player.rect.y = 768-45-120
+   sprites.add(player)
 
 #add enemy to group
 for i in range(50):
-    p1 = Enemy(RED, 20, 15)
-    p1.rect.x = 1024-10-44
-    p1.rect.y = 768-45-120
-    sprites.add(p1)
+   enemy = Enemy(RED, 20, 15)
+   enemy.rect.x = 1024-10-44
+   enemy.rect.y = 768-45-120
+   sprites.add(enemy)
 
 #add ball to group
 for i in range(50):
-    p1 = Ball(RED, 20, 15)
-    p1.rect.x = 10+44-20+5
-    p1.rect.y = 768-45-44-30-20
-    sprites.add(p1)
+    ball = Ball(RED, 20, 15)
+    ball.rect.x = 10+44-20+5
+    ball.rect.y = 768 - 45 - 44 - 30 - 20
+    sprites.add(ball)
 
-started = False
+#Window Classes
+class LevelSelect:
+    func = 0
+    def __init__(self, master):
+        def funcTo(num):
+            LevelSelect.func = num
+            print(LevelSelect.func)
+            root.withdraw()
+            root.destroy()
 
-while True: # main game loop
+        self.master = master
+        master.title("A simple GUI")
+        self.label = Label(master, text="Choose")
+        self.label.pack()
+        self.greet_button = Button(master, text="level one", command=lambda: funcTo(1))
+        self.greet_button.pack()
+        self.greet_button = Button(master, text="level two", command=lambda: funcTo(2))
+        self.greet_button.pack()
 
+
+class SelectEquation:
+    func = 0
+
+    def __init__(self, master):
+        def funcTo(num):
+            SelectEquation.func = num
+            print(SelectEquation.func)
+            root2.withdraw()
+            root2.destroy()
+        self.master = master
+        master.title("A simple GUI")
+        self.label = Label(master, text="Choose")
+        self.label.pack()
+        self.greet_button = Button(master, text="line", command= lambda: funcTo(1))
+        self.greet_button.pack()
+        self.greet_button = Button(master, text="Parabola", command= lambda: funcTo(2))
+        self.greet_button.pack()
+        self.greet_button = Button(master, text="Exponential", command= lambda: funcTo(3))
+        self.greet_button.pack()
+        self.close_button = Button(master, text="OK", command=master.quit)
+        self.close_button.pack()
+
+class LoseWindow:
+    func = 0
+
+    def __init__(self, master):
+        def funcTo(num):
+            LoseWindow.func = num
+            print(LoseWindow.func)
+            master.quit
+            root.withdraw()
+
+        self.master = master
+        master.title("A simple GUI")
+        self.label = Label(master, text="Do you want to leave the game?")
+        self.label.pack()
+        self.greet_button = Button(master, text="Play Again!", command=lambda: funcTo(1))
+        self.greet_button.pack()
+        self.greet_button = Button(master, text="Quit", command=lambda: funcTo(2))
+        self.greet_button.pack()
+
+def showFire():
+    for i in range(50):
+        enemy_hit = Enemy_Hit(RED, 20, 15)
+        enemy_hit.rect.x = 1024 - 95
+        enemy_hit.rect.y = 768 - 45 - 120
+        hitsprites.add(enemy_hit)
+        hitsprites.draw(screen)
+        pygame.display.update()
+        pygame.display.flip()
+        pygame.time.delay(3000)
+        screen.fill(WHITE)
+        screen.blit(bg, (0, 0))
+        pygame.draw.rect(screen, (0, 0, 0), (0, 768 - 45, 1024, 45))
+        sprites.draw(screen)
+        pygame.display.update()
+
+#playing function
+def play():
+    hit = False
+    while True:
+        while hit == False:  # main game loop
+            screen.fill(WHITE)
+            screen.blit(bg, (0, 0))
+            pygame.draw.rect(screen, (0, 0, 0), (0, 768 - 45, 1024, 45))
+            hit = line(ball, enemy, coeff)
+            if hit:
+                showFire()
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+            pygame.display.update()
+
+def line(ball, enemy, coeff):
+    starttime = pygame.time.get_ticks()
     screen.fill(WHITE)
-    screen.blit(bg, (0,0))
-    pygame.draw.rect (screen, (0, 0, 0), (0, 768-45, 1024, 45))
+    screen.blit(bg, (0, 0))
+    pygame.draw.rect(screen, (0, 0, 0), (0, 768 - 45, 1024, 45))
     sprites.draw(screen)
-
-
-    #test.move_player()
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
     pygame.display.update()
-    if started == False:
-        root = Tk()
-        app = App(root)
+    go = True
+    while go:
+        currenttime = pygame.time.get_ticks()
+        if currenttime-starttime < 6000:
+            ball.rect.x += 1
+            ball.rect.y += -coeff
+        else:
+            ball.rect.x = ball.rect.x
+            ball.rect.y = ball.rect.y
+            easygui.msgbox("You missed the target!", title="Wrong")
+            go = False
+            return False
+        screen.fill(WHITE)
+        screen.blit(bg, (0, 0))
+        pygame.draw.rect(screen, (0, 0, 0), (0, 768 - 45, 1024, 45))
+        sprites.draw(screen)
+        pygame.display.update()
+        pygame.display.flip()
+        if (ball.rect.x >= enemy.rect.x and ball.rect.x<= enemy.rect.x+44) and (ball.rect.y >= enemy.rect.y and ball.rect.y <= enemy.rect.y+120):
+            go = False
+            return True
+
+root = tk.Tk()
+app = LevelSelect(root)
+
+root2 = tk2.Tk()
+app2 = SelectEquation(root2)
+
+started1 = False
+started2 = False
+started3 = False
+leave = False
+#Main Loop
+
+while leave == False:
+    if started1 == False:
+        started1 = True
+        LevelSelect.func = 1
         root.mainloop()
-        started = True
-    root.withdraw()
+        if LevelSelect.func == 1:
+            print(SelectEquation.func)
+            root2.mainloop()
+            if SelectEquation.func == 1:
+                play()
+            elif SelectEquation.func == 2:
+                play()
+            elif SelectEquation.func == 3:
+                play()
+
+        elif LevelSelect.func == 2:
+            root2.mainloop()
+            if SelectEquation.func == 1:
+                play()
+            elif SelectEquation.func == 2:
+                play()
+            elif SelectEquation.func == 3:
+                play()
+
+        else: print("nada")
