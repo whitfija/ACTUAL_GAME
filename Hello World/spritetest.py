8346b6bd2d9365be4cd770bb36b893eec3ddde92
DylanDillpickle <33379133+DylanDillpickle@users.noreply.github.com>
2017-11-05 00:08:53 -0400

    adisfhlasdi jfhasdlf ojasdnf

--- Hello World/spritetest.py
+++ Hello World/spritetest.py
@@ -61,12 +61,12 @@ class Ball(pygame.sprite.Sprite):
         # define rect
         self.rect = self.image.get_rect()
 
-class Ground(pygame.sprite.Sprite):
-    def __init__(self, color, width, height):
-        super().__init__()
-        self.image = pygame.image.load("Ground.png").convert()
-        self.image.set_colorkey(WHITE)
-        self.rect = self.image.get_rect()
+#class Ground(pygame.sprite.Sprite):
+ #   def __init__(self, color, width, height):
+  #      super().__init__()
+   #     self.image = pygame.image.load("Ground.png").convert()
+    #    self.image.set_colorkey(WHITE)
+     #   self.rect = self.image.get_rect()
 #start pygame & set display
 pygame.init()
 screen = pygame.display.set_mode((1024,768))
@@ -75,35 +75,36 @@ pygame.display.set_caption('Sprite Test')
 sprites = pygame.sprite.Group()
 
 #add player to group
-for i in range(50):
-    ground = Ground(RED, 20, 15)
-    ground.rect.x = 0
-    ground.rect.y = 677
-    sprites.add(ground)
+#for i in range(50):
+ #   ground = Ground(RED, 20, 15)
+    #ground.rect.x = 0
+   # ground.rect.y = 0
+  #  sprites.add(ground)
 
 for i in range(50):
     p1 = Player(RED, 20, 15)
     p1.rect.x = 10
-    p1.rect.y = 10
+    p1.rect.y = 768-45-120
     sprites.add(p1)
 
 #add enemy to group
 for i in range(50):
     p1 = Enemy(RED, 20, 15)
-    p1.rect.x = 10
-    p1.rect.y = 210
+    p1.rect.x = 1024-10-44
+    p1.rect.y = 768-45-120
     sprites.add(p1)
 
 #add ball to group
 for i in range(50):
     p1 = Ball(RED, 20, 15)
-    p1.rect.x = 10
-    p1.rect.y = 410
+    p1.rect.x = 10+44-20+5
+    p1.rect.y = 768-45-44-30-20
     sprites.add(p1)
 
 while True: # main game loop
     screen.fill(WHITE)
     screen.blit(bg, (0,0))
+    pygame.draw.rect (screen, (0, 0, 0), (0, 768-45, 1024, 45))
     sprites.draw(screen)
     #test.move_player()
 
