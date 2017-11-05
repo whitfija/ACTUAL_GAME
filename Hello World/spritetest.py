a037528fe629636bf7891fe79e2ab6d7715ebc9b
3two <whitfija@gmail.com>
2017-11-04 23:43:15 -0400

    GRIDDIDDOO

--- Hello World/spritetest.py
+++ Hello World/spritetest.py
@@ -7,6 +7,8 @@ BLACK = (0,   0,   0)
 WHITE = (255, 255, 255)
 RED = (255,   0,   0)
 
+bg = pygame.image.load("grid.png")
+
 #Player class
 class Player(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
@@ -60,7 +62,7 @@ class Ball(pygame.sprite.Sprite):
 
 #start pygame & set display
 pygame.init()
-screen = pygame.display.set_mode((1500,750))
+screen = pygame.display.set_mode((1024,768))
 pygame.display.set_caption('Sprite Test')
 #create sprites group
 sprites = pygame.sprite.Group()
@@ -88,6 +90,7 @@ for i in range(50):
 
 while True: # main game loop
     screen.fill(WHITE)
+    screen.blit(bg, (0,0))
     sprites.draw(screen)
     #test.move_player()
 
