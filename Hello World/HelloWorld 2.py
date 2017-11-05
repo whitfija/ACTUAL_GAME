fe66e8ea215d9b50593d7dcdd408334430361495
DylanDillpickle <33379133+DylanDillpickle@users.noreply.github.com>
2017-11-04 21:30:49 -0400

    NEW ENEMY

--- Hello World/HelloWorld 2.py
+++ Hello World/HelloWorld 2.py
@@ -1,17 +1,17 @@
 import pygame, sys
 from pygame.locals import *
 from sys import exit
-from
+from sprite_classes import player
+
 pygame.init()
 screen = pygame.display.set_mode((500,500))
-
-pygame.sprite.Sprite.add
 
 def screen():
     pygame.draw.rect(screen, (250, 250, 250), (0, 0, 500, 500))
 
 def drawplayer():
-    pygame.display.set_caption('Hello World!')
+    pygame.draw.player(drawplayer, (0,0,0), ())
+
 while True: # main game loop
     drawplayer()
     drawscreen()
--- Hello World/sprite classes.py
+++ Hello World/sprite classes.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
-class Player(pygame.sprite.Sprite):
+class player(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         super().__init__()
-        self = pygame.image.load
+        self = pygame.image.load(os.path.join('player', 'Player.png'))
