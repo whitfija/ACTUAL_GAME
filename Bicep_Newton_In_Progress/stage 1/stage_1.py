7d9ed3c710d04aa3c6a67d56e58d0efc4f2ad879
3two <whitfija@gmail.com>
2017-11-05 11:22:32 -0500

    nbhuki,

--- Bicep_Newton_In_Progress/stage 1/stage_1.py
+++ Bicep_Newton_In_Progress/stage 1/stage_1.py
@@ -1,4 +1,4 @@
-import pygame, sys, math, easygui
+import pygame, sys, math, easygui, os, time
 from pygame.locals import *
 import time
 from sys import exit
@@ -76,8 +76,10 @@ class Enemy_Hit(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
 # start pygame & set display
-screen = pygame.display.set_mode((1024, 768))
+screen = pygame.display.set_mode((1024, 768),0,0)
 pygame.display.set_caption('Level 1')
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
+time.sleep(2)
 # create sprites group
 sprites = pygame.sprite.Group()
 hitsprites = pygame.sprite.Group()
@@ -213,9 +215,10 @@ def line(ball, enemy, coeff):
     pygame.display.update()
     go = True
     while go:
-        currenttime = pygame.time.get_ticks()
-        if currenttime-starttime < 6000:
-            ball.rect.x += 1
+        currenttime = pygame.time.Clock()
+        main_clock.tick(100)
+        if currenttime-starttime < 2000:
+            ball.rect.x += 2
             ball.rect.y += -coeff
         else:
             ball.rect.x = ball.rect.x
@@ -238,6 +241,8 @@ app = LevelSelect(root)
 
 root2 = tk2.Tk()
 app2 = SelectEquation(root2)
+
+
 
 started1 = False
 started2 = False
