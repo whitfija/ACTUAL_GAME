9bf103f0ebfa02ce58ec0a80b867df3abd237175
kingderpie <purpleman280@gmail.com>
2017-11-05 02:35:00 -0500

    working but and meth

--- Bicep_Newton_In_Progress/stage 1/stage_1.py
+++ Bicep_Newton_In_Progress/stage 1/stage_1.py
@@ -3,7 +3,16 @@ from pygame.locals import *
 from sys import exit
 from tkinter import Tk, Label, Button
 
+func = 0
+def funcTo1():
+    func = 1
+def funcTo2():
+    func = 2
+def funcTo3():
+    func = 3
 class App:
+
+
     def __init__(self, master):
         self.master = master
         master.title("A simple GUI")
@@ -11,13 +20,13 @@ class App:
         self.label = Label(master, text="Choose")
         self.label.pack()
 
-        self.greet_button = Button(master, text="SQRT", command=master.quit)
+        self.greet_button = Button(master, text="SQRT", command=funcTo1)
         self.greet_button.pack()
 
-        self.greet_button = Button(master, text="Parabola", command=master.quit)
+        self.greet_button = Button(master, text="Parabola", command=funcTo2)
         self.greet_button.pack()
 
-        self.greet_button = Button(master, text="Exponential", command=master.quit)
+        self.greet_button = Button(master, text="Exponential", command=funcTo3)
         self.greet_button.pack()
 
         self.close_button = Button(master, text="OK", command=master.quit)
@@ -127,4 +136,4 @@ while True: # main game loop
         app = App(root)
         root.mainloop()
         started = True
-    else app.
+    root.withdraw()
